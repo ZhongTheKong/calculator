186abf6ea5ea968eddcc1d57521dc72f157a200b
ZhongTheKong <guoyizhong2004@gmail.com>
2025-01-06 18:03:41 -0500

    Fixed subtract and added divide

--- models/Calculator.cpp
+++ models/Calculator.cpp
@@ -5,9 +5,13 @@ double Calculator::add(double num1, double num2) {
 }
 
 double Calculator::subtract(double num1, double num2) {
-    return num1 / num2;
+    return num1 - num2;
 }
 
 double Calculator::multiply(double num1, double num2) {
     return num1 * num2;
+}
+
+double Calculator::divide(double num1, double num2) {
+    return num1 / num2;
 }
